4f2015519d335ec0e1f2f3aedeb72ef739518a50
ZDy <975313032@qq.com>
2022-09-26 18:33:43 +0800

    第4次提交

--- Spring_Boot/SpringBoot_Demo02_web/src/main/java/com/zhou/boot/controller/HelloController.java
+++ Spring_Boot/SpringBoot_Demo02_web/src/main/java/com/zhou/boot/controller/HelloController.java
@@ -1,7 +1,20 @@
 package com.zhou.boot.controller;
 
+import com.zhou.boot.bean.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/test/user")
+    public User user(){
+        User user = new User();
+        user.setAge(28);
+        user.setName("lisi");
+        return user;
+    }
 }
--- Spring_Boot/SpringBoot_Demo02_web/src/main/java/com/zhou/boot/converter/ZhouMessageConverter.java
+++ Spring_Boot/SpringBoot_Demo02_web/src/main/java/com/zhou/boot/converter/ZhouMessageConverter.java
@@ -1,5 +1,6 @@
 package com.zhou.boot.converter;
 
+import com.zhou.boot.bean.User;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
@@ -8,31 +9,46 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 
-public class ZhouMessageConverter implements HttpMessageConverter {
+
+/**
+ * 自定义Converter
+ */
+public class ZhouMessageConverter implements HttpMessageConverter<User> {
     @Override
-    public boolean canRead(Class clazz, MediaType mediaType) {
+    public boolean canRead(Class<?> clazz, MediaType mediaType) {
         return false;
     }
 
     @Override
-    public boolean canWrite(Class clazz, MediaType mediaType) {
-        return false;
+    public boolean canWrite(Class<?> clazz, MediaType mediaType) {
+        return clazz.isAssignableFrom(User.class);
     }
 
+    /**
+     * 服务器要统计所有MessageConverter都能写出哪些内容类型
+     * application/zhou
+     * @return
+     */
     @Override
     public List<MediaType> getSupportedMediaTypes() {
+        return MediaType.parseMediaTypes("application/zhou");
+    }
+
+    @Override
+    public User read(Class<? extends User> clazz, HttpInputMessage inputMessage) throws IOException, HttpMessageNotReadableException {
         return null;
     }
 
     @Override
-    public Object read(Class clazz, HttpInputMessage inputMessage) throws IOException, HttpMessageNotReadableException {
-        return null;
-    }
+    public void write(User user, MediaType contentType, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
+        //自定义协议数据的写出
+        String data = user.getName()+";"+user.getAge();
 
-    @Override
-    public void write(Object o, MediaType contentType, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
-
+        //写出去
+        OutputStream body = outputMessage.getBody();
+        body.write(data.getBytes());
     }
 }
